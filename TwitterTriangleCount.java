21fc2d0472022ed42e99f7e9bcfbea3e167cee9e
reeechart <ferdinandusrichard@yahoo.co.id>
2018-11-17 19:07:52 +0700

    add followers mapper and reducer

--- TwitterTriangleCount.java
+++ TwitterTriangleCount.java
@@ -1,9 +1,11 @@
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.StringTokenizer;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.ArrayWritable;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
@@ -16,6 +18,27 @@ import org.apache.hadoop.util.GenericOptionsParser;
 
 public class TwitterTriangleCount {
 	private static final String INTERMEDIATE_OUTPUT_PATH_1 = "intermediate_output_one";
+
+	public static class LongArrayWritable extends ArrayWritable {
+		public LongArrayWritable(LongWritable[] values) {
+			super(LongWritable.class, values);
+		}
+
+		@Override
+		public LongWritable[] get() {
+			return (LongWritable[]) super.get();
+		}
+
+		@Override
+		public String toString() {
+			String result = "";
+			LongWritable[] values = get();
+			for (int i = 0; i < values.length; i++) {
+				result = result + " " + values[i].toString();
+			}
+			return result;
+		}
+	}
 
 	public static class UndirectedGraphMapper extends Mapper<LongWritable, Text, LongWritable, LongWritable> {
 		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
@@ -46,11 +69,36 @@ public class TwitterTriangleCount {
 	public static class UndirectedGraphReducer extends Reducer<LongWritable, LongWritable, LongWritable, LongWritable> {
 		public reduce(LongWritable key, Iterable<LongWritable> values, Context context) throws IOException, InterruptedException {
 			Set<LongWritable> uniqueFollowers = HashSet<LongWritable>();
+
 			for (LongWritable value : values) {
 				uniqueFollowers.add(value);
 			}
 			for (LongWritable uniqueFollower : uniqueFollowers) {
 				context.write(key, uniqueFollower);
+			}
+		}
+	}
+
+	public static class FollowersMapper extends Mapper<LongWritable, LongWritable, LongWritable, LongWritable> {
+		public map(LongWritable key, LongWritable value Context context) throws IOException, InterruptedException {
+			context.write(key, value);
+		}
+	}
+
+	public static class FollowersReducer extends Reducer<LongWritable, LongWritable, LongWritable, TupleWritable> {
+		public reduce(LongWritable key, Iterable<LongWritable>, Context context) throws IOException, InterruptedException {
+			LongWritable[] followerTuple = new LongWritable[2];
+			List<LongWritable> followersList = new ArrayList<LongWritable>();
+
+			for (LongWritable followerOne : values) {
+				followerTuple[0] = followerOne;
+				for (LongWritable followerTwo: values) {
+					followerTuple[1] = followerTwo;
+					if (followerOne.compareTo(followerTwo) != 0) {
+						TupleWritable tuple = new TupleWritable(followerTuple);
+						context.write(key, followerTuple);
+					}
+				}
 			}
 		}
 	}
